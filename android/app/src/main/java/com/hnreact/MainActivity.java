5e41dbe878bc79c67ed1324a5b87fe20c91f89c9
Kyle Corbitt <kyle@corbt.com>
2015-09-25 18:08:30 +0100

    rename android package

--- android/app/src/main/java/com/hnreact/MainActivity.java
+++ android/app/src/main/java/com/hnreact/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hnreact;
+package com.corbt.hnreact;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,7 +9,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
 
