ef68985dd3ef09b2b5d2c3928c00ad1fd7b09103
Kyle Corbitt <kyle@corbt.com>
2015-09-30 22:16:05 +0100

    HeaderScrollView now takes into account header height

--- app/components/HeaderScrollView.js
+++ app/components/HeaderScrollView.js
@@ -13,7 +13,7 @@ let {
 module.exports = class HeaderScrollView extends Component {
   constructor() {
     super();
-    this.state = {headerHeight: 100, top: 0, scrollOffset: 0}
+    this.state = {headerHeight: 0, top: 0, scrollOffset: 0}
   }
 
   reactToScroll(e) {
@@ -26,22 +26,18 @@ module.exports = class HeaderScrollView extends Component {
     // top >= -headerHeight
     // top >= -scrollOffset
     stateUpdate.top = Math.max(Math.min(this.state.top - deltaOffset, 0), -this.state.headerHeight, -scrollOffset);
-    // console.log("update", stateUpdate);
     this.setState(stateUpdate);
   }
 
-  componentDidMount() {
-    this.refs.test.measure((ox, oy, width, height, px, py) => {
-      console.log("measured", ox, oy, width, height, px, py);
-    });
-  }
-
   render() {
-    // console.log("state", this.state);
     let { style, header, children, ...props } = this.props;
     return <View style={style}>
-      <ScrollView {...props} style={[style, {top: 0, bottom: 0, left: 0, right: 0, position: 'absolute', paddingTop: this.state.headerHeight}]} onScroll={this.reactToScroll.bind(this)}>{children}</ScrollView>
-      <View ref="test" style={{position: 'absolute', left: 0, right: 0, top: this.state.top}}>
+      <ScrollView {...props}
+        style={[style, {top: 0, bottom: 0, left: 0, right: 0, position: 'absolute', paddingTop: this.state.headerHeight}]}
+        onScroll={this.reactToScroll.bind(this)}>
+        {children}
+      </ScrollView>
+      <View onLayout={event => this.setState({headerHeight: event.nativeEvent.layout.height})} style={{position: 'absolute', left: 0, right: 0, top: this.state.top}}>
         {header}
       </View>
     </View>;
